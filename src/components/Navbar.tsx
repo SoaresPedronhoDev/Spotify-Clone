65ea463f0ea68ad6ec98da85f3ca4dadd56e30b7
Pedro Henrique <pedrohenriquedevprog@gmail.com>
2024-09-23 20:59:20 -0300

    Arquivo Navbar feito

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,8 +11,15 @@ export default function Navbar(){
                 <img className="w-8 bg-black p-2 rounded-2x1 cursor-pointer" src={assets.arrow_right} alt="Arrow Right Icon"></img>
             </div>
             <div className="flex items-center gap-4">
-                <p className="bg-white text-black text-[15px] px-4 py-1 rounded-2x1 hidden md:block">Explore Premium</p>
+                <p className="bg-white text-black text-[15px] px-4 py-1 rounded-2x1 hidden md:block cursor-pointer">Explore Premium</p>
+                <p className="bg-black py-1 px-3 rounded-2x1 text-[15px] cursor-pointer">Install App</p>
+                <p className="bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center">P</p>
             </div>
+        </div>
+        <div className="flex items-center gap-2 mt-4">
+            <p className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer">All</p>
+            <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">Music</p>
+            <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">Podcasts</p>
         </div>
         </>
     )
