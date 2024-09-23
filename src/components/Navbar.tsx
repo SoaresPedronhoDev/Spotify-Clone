f6b67386e089bf93107227f8e2d4fdb2ee2665e1
Pedro Henrique <pedrohenriquedevprog@gmail.com>
2024-09-23 20:45:23 -0300

    botao explore premium adicionado

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,6 +10,9 @@ export default function Navbar(){
                 <img className="w-8 bg-black p-2 rounded-2x1 cursor-pointer" src={assets.arrow_left} alt="Arrow Left Icon"></img>
                 <img className="w-8 bg-black p-2 rounded-2x1 cursor-pointer" src={assets.arrow_right} alt="Arrow Right Icon"></img>
             </div>
+            <div className="flex items-center gap-4">
+                <p className="bg-white text-black text-[15px] px-4 py-1 rounded-2x1 hidden md:block">Explore Premium</p>
+            </div>
         </div>
         </>
     )
