397cd577d69d38ddfedb81e95e5d987af822b14f
Pedro Henrique <pedrohenriquedevprog@gmail.com>
2024-09-23 21:19:45 -0300

    Imagems aparecendo com suas informacoes

--- src/components/DisplayHome.tsx
+++ src/components/DisplayHome.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { albumsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
 
 
 export default function DisplayHome(){
     return(
         <>
         <Navbar/>
+        <div className="my-5 font-bold text-2x1">
+            {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} image={item.image}/>))}
+        </div>
         </>
     )
 }
