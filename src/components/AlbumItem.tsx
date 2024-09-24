2a1c40244c63e3f0419c83ede2f85d1f6deaf284
Pedro Henrique <pedrohenriquedevprog@gmail.com>
2024-09-24 20:08:35 -0300

    criado arquivo Displalbum com rota e navbar adicionada nele

--- src/components/AlbumItem.tsx
+++ src/components/AlbumItem.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import {  useNavigate } from "react-router-dom";
 
 
-export default function AlbumItem({ image, name, desc }: { image: string, name: string, desc: string }){
+export default function AlbumItem({ image, name, desc,id }: { image: string, name: string, desc: string, id : number }){
+
+    const navigate = useNavigate()
+
     return(
         <>
-        <div className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
+        <div onClick={() => navigate(`/album/${id}`)} className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
             <img className='rounded' src={image} alt=""></img>
             <p className="font-bold mt-2 mb-2">{name}</p>
             <p className="text-slate-200 text-sm">{desc}</p>
--- src/components/DisplayHome.tsx
+++ src/components/DisplayHome.tsx
@@ -13,7 +13,7 @@ export default function DisplayHome(){
         <div className="mb-4">
             <h1 className="my-5 font-bold text-2xl">Features Charts</h1>
             <div className="flex overflow-auto">
-                {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} image={item.image}/>))}
+                {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} image={item.image} id={item.id}/>))}
             </div>
         </div>
         <div className="mb-4">
