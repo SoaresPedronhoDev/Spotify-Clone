6b93d04c4fc2a036bcc0edbd7e7952f1856bcc31
Pedro Henrique <pedrohenriquedevprog@gmail.com>
2024-10-02 20:51:41 -0300

    adicionado troca de background colors em albums

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,24 +1,31 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import DisplayHome from "./DisplayHome";
 import DisplayAlbum from "./DisplayAlbum";
+import { albumsData } from "../assets/assets";
 
+export default function Display() {
+  const displayRef = useRef<HTMLDivElement>(null);
+  const location = useLocation();
+  const isAlbum = location.pathname.includes("album");
+  const albumId = isAlbum ? location.pathname.slice(-1) : "";
+  const albumIndex = Number(albumId);
 
-export default function Display(){
+  useEffect(() => {
+    if (isAlbum && !isNaN(albumIndex) && albumsData[albumIndex]) {
+      const bgColor = albumsData[albumIndex].bgColor;
+      displayRef.current!.style.background = `linear-gradient(${bgColor}, #121212)`;
+    } else {
+      displayRef.current!.style.background = "#121212";
+    }
+  }, [isAlbum, albumIndex]); // Dependências para evitar renderizações desnecessárias
 
-    const displayRef = useRef<HTMLDivElement>(null);
-    const location = useLocation()
-    const isAlbum = location.pathname.includes("album")
-    console.log(isAlbum)
-
-    return(
-        <>
-        <div ref={displayRef} className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
-            <Routes>
-                <Route path="/" element={<DisplayHome/>}/>
-                <Route path="/album/:id" element={<DisplayAlbum/>}/>
-            </Routes>
-        </div>
-        </>
-    )
+  return (
+    <div ref={displayRef} className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
+      <Routes>
+        <Route path="/" element={<DisplayHome />} />
+        <Route path="/album/:id" element={<DisplayAlbum />} />
+      </Routes>
+    </div>
+  );
 }
